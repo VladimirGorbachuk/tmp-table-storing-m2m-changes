bace2a1da674894955017355357861820881401a
vladimirgorbachuk@gmail.com <vladimirgorbachuk@gmail.com>
2024-01-28 21:43:53 +0300

    get form - tmp selected set to current group users

--- dj/some/admin.py
+++ dj/some/admin.py
@@ -16,6 +16,13 @@ class GroupNewAdmin(admin.ModelAdmin):
         current_user = request.user
         tmp_selected = TmpGroupForFillingWithUsers.objects.get(author=current_user)
         super().save_model(request, obj, form, change)
-        for user in tmp_selected.users.all():
-            user.groups.add(obj)
+        obj.user_set.set(tmp_selected.users.all())
         tmp_selected.delete()
+
+    def get_form(self, request, obj=None, **kwargs):
+        if request.method == 'GET':
+            current_user = request.user
+            tmp_selected, _ = TmpGroupForFillingWithUsers.objects.get_or_create(author=current_user)
+            if obj:
+                tmp_selected.users.set(User.objects.filter(groups=obj))
+        return super().get_form(request, obj, **kwargs)
